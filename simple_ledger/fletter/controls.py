9c7cea1beba5aeef0b0fa00260a43e50d3c829dd
Lucifer516-sudo <harishvengat02092005@gmail.com>
2023-05-19 20:50:44 +0530

    Update: Using Paginated Data Table that is on github

--- simple_ledger/fletter/controls.py
+++ simple_ledger/fletter/controls.py
@@ -1,170 +1,214 @@
 import flet as ft
+import math
 from simple_ledger import flet_logger, ledger_logger, Logger
+from simple_ledger.db import LedgerDB
 
 
 class PaginatedDataTable(ft.UserControl):
+    DEFAULT_ROW_PER_PAGE: int = 10
+
     def __init__(
         self,
         columns: list[ft.DataColumn] | None = None,
         rows: list[ft.DataRow] | None = None,
-        default_number_of_rows: int = 10,
+        table_title: str | None = None,
+        rows_per_page: int = DEFAULT_ROW_PER_PAGE,
     ):
-        super().__init__()
+        self.columns: list[ft.DataColumn] | None = columns
+        self.rows: list[ft.DataRow] | None = rows
+        self.table_title: str | None = table_title
+        self.rows_per_page: int = rows_per_page
 
-        self.columns: list[ft.DataColumn] | None | list[str] = columns
-        self.rows: list[ft.DataRow] | None | list[list[str]] = rows
-        self.default_no_of_rows: int = default_number_of_rows
-        self.current_page_number: int = 0
-        flet_logger.info(f"Creating CustomControl: `Paginated Data Table`")
+        self.current_page: int = 1  # default page
+        self.total_pages: int = 1  # total number of pages
+        self.paginated_rows = self.paginated_rows
+
+        # controls
+
+        self.page_number_display: ft.Text = ft.Text(
+            value=f"{self.current_page}/{self.total_pages}",
+            style=ft.TextThemeStyle.DISPLAY_MEDIUM,
+            italic=True,
+        )
+        self.next_page_button: ft.IconButton = ft.IconButton(
+            icon=ft.icons.KEYBOARD_ARROW_RIGHT_ROUNDED,
+            tooltip="Next Page",
+            on_click=self._on_next_page(),
+        )
+        self.prev_page_button: ft.IconButton = ft.IconButton(
+            icon=ft.icons.KEYBOARD_ARROW_LEFT_ROUNDED,
+            tooltip="Previous Page",
+            on_click=self._on_prev_page(),
+        )
+
+        self.first_page_button: ft.IconButton = ft.IconButton(
+            icon=ft.icons.KEYBOARD_DOUBLE_ARROW_LEFT_ROUNDED,
+            tooltip="First Page",
+            on_click=self._on_first_page,
+        )
+        self.last_page_button: ft.IconButton = ft.IconButton(
+            icon=ft.icons.KEYBOARD_DOUBLE_ARROW_RIGHT_ROUNDED,
+            tooltip="Last Page",
+            on_click=self._on_last_page,
+        )
+
+        self.title_text: ft.Text = ft.Text(
+            value=self.table_title.title(), style=ft.TextThemeStyle.TITLE_MEDIUM
+        )
+        self.refresh_data_button: ft.IconButton = ft.IconButton(
+            icon=ft.icons.REFRESH_SHARP,
+            tooltip="Refresh Data",
+            on_click=self._on_refresh(),
+        )
+
+        self.data_table = ft.DataTable(
+            columns=self.columns,
+            heading_text_style=ft.TextStyle(
+                size=10, italic=True, decoration=ft.TextDecoration.UNDERLINE
+            ),
+            heading_row_color=ft.colors.BACKGROUND,
+            show_bottom_border=True,
+        )
+
+        if self.is_rows == True:
+            if len(list(self.paginated_rows.keys())) >= 1:
+                self.data_table.rows = self.paginated_rows[1]
+                self.update()
 
     @property
-    def total_pages(self) -> int:
-        if (self.rows != None) and (len(self.rows) > self.default_no_of_rows):
-            flet_logger.info("Returning Total Number of Pages")
-            flet_logger.debug(
-                f"Number of rows > 0 (!None) => Rows: {self.rows} & Default Rows: {self.default_no_of_rows}"
-            )
-            return len(self.rows) // self.default_no_of_rows
+    def is_columns(self) -> bool:
+        if self.columns != None:
+            return True
         else:
-            return 1
+            return False
 
     @property
-    def paginated_rows(self) -> dict[int, list[ft.DataRow]] | None:
-        # using list comprehension
-        if (self.rows != None) and (len(self.rows) > 0):
-            flet_logger.info("Creating Paginated Rows")
-            _paginated_rows: list[ft.DataRow] = [
-                self.rows[
-                    i * self.default_no_of_rows : (i + 1) * self.default_no_of_rows
-                ]
-                for i in range(
-                    (len(self.rows) + self.default_no_of_rows - 1)
-                    // self.default_no_of_rows
-                )
-            ]
-            flet_logger.debug(
-                f"Splitted Rows into paginated rows \nPaginated Row: {len(_paginated_rows)}"
-            )
-
-            paginated_data: dict[int, list[ft.DataRow]] = {}
-
-            for page_num in range(len(_paginated_rows)):
-                paginated_data[page_num + 1] = _paginated_rows[page_num]
-                flet_logger.info("Creating Paginated Data Dict")
-                flet_logger.debug(
-                    f"Page {page_num+1} => {len(_paginated_rows[page_num])}"
-                )
-            flet_logger.info("Returning Paginated Data")
-            return paginated_data
+    def is_rows(self) -> bool:
+        if self.rows != None:
+            return True
         else:
-            flet_logger.warning(f"Unable To Create Paginated data")
-            flet_logger.critical(
-                f"Rows => {self.rows}\nPaginated Rows => {_paginated_rows}\nPaginated Data => {paginated_data}"
-            )
+            return False
+
+    @property
+    def total_rows(self) -> int | None:
+        if self.is_rows == True:
+            return len(self.rows)
+        else:
             return None
 
-    def build(self):
-        def _on_increase(e):
-            self.current_page_number += 1
-            if self.current_page_number < self.total_pages:
-                flet_logger.info("Increasing the Page")
-                flet_logger.debug(
-                    f"Current Page => {self.current_page_number}\nTotal Page => {self.total_pages}\nPage Display Value => {page_number_display.value}"
-                )
-                page_number_display.value = (
-                    f"{self.current_page_number} of {self.total_pages}"
-                )
-                try:
-                    table.rows = self.paginated_rows[self.current_page_number]
-                    flet_logger.debug(f"Updating Table Rows")
-                except Exception as e:
-                    flet_logger.exception(f"Encountered Exception: \n{e}")
-                self.update()
-            else:
-                flet_logger.info("Not Increasing the Page")
-                flet_logger.debug(
-                    f"Current Page => {self.current_page_number}\nTotal Page => {self.total_pages}\nPage Display Value => {page_number_display.value}"
-                )
+    def paginate_rows(self) -> dict[int, list[ft.DataRow]] | None:
+        if self.total_rows != None:
+            _ = {}
 
-        def _on_decrease(e):
-            self.current_page_number -= 1
-            if self.current_page_number > 0:
-                flet_logger.info("Decreasing the Page")
-                flet_logger.debug(
-                    f"Current Page => {self.current_page_number}\nTotal Page => {self.total_pages}\nPage Display Value => {page_number_display.value}"
+            _temp_sub_list: list[
+                ft.DataRow
+            ] = []  # create a list to paginated chunks (rows)
+            shallow_copy_of_rows = self.rows.copy()  # making a shallow copy of list
+            for chunk_index in range(0, self.total_rows, self.rows_per_page):
+                _temp_sub_list = shallow_copy_of_rows[
+                    chunk_index : chunk_index + self.rows_per_page
+                ]
+
+            for _page_num in range(self.total_rows):
+                _[_page_num + 1] = _temp_sub_list[
+                    _page_num
+                ]  # creating the paginated data dict , that has the page num and the rows
+
+            return _
+        else:
+            return None
+
+    @property
+    def get_total_pages(self) -> int | None:
+        float_part, int_part = math.modf(self.total_rows / self.rows_per_page)
+        if float_part > 0.0:
+            int_part += 1
+            return int_part
+        elif (float_part < 0.0) or (float_part == 0):
+            return int_part
+        else:
+            return None
+
+    def set_page(self, delta: int = 0):
+        self.current_page += delta
+        self.page_number_display.value = f"{self.current_page}/{self.total_pages}"
+        self.data_table.rows = self.paginated_rows[self.current_page]
+        self.update()
+
+    def set_a_page(self, page_number: int = 1):
+        self.current_page = 1
+        self.page_number_display.value = f"{self.current_page}/{self.total_pages}"
+        self.data_table.rows = self.paginated_rows[self.current_page]
+        self.update()
+
+    def _on_next_page(self, e: ft.ControlEvent):
+        if self.current_page <= self.total_pages:
+            self.set_page(delta=1)
+
+    def _on_prev_page(self, e: ft.ControlEvent):
+        if self.current_page >= 1:
+            self.set_page(delta=-1)
+
+    def _on_first_page(self, e: ft.ControlEvent):
+        if self.total_pages >= 1:
+            self.set_a_page(page_number=1)
+
+    def _on_last_page(self, e: ft.ControlEvent):
+        if self.total_pages >= 1:
+            self.set_a_page(page_number=self.total_pages)
+
+    def _on_refresh(self):
+        db = LedgerDB()
+        required_data = []
+
+        for data in db.read_ledger_info():
+            required_data.append(
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text(value=str(data.id))),
+                        ft.DataCell(ft.Text(value=str(data.from_person))),
+                        ft.DataCell(ft.Text(value=str(data.to_person))),
+                        ft.DataCell(ft.Text(value=str(data.amount))),
+                        ft.DataCell(ft.Text(value=str(data.tag))),
+                    ]
                 )
-                page_number_display.value = (
-                    f"{self.current_page_number} of {self.total_pages}"
-                )
-                try:
-                    table.rows = self.paginated_rows[self.current_page_number]
-                    flet_logger.debug(f"Updating Table Rows")
-                except Exception as e:
-                    flet_logger.exception(f"Encountered Exception: \n{e}")
-                self.update()
-            else:
-                flet_logger.info("Not Decreasing the Page")
-                flet_logger.debug(
-                    f"Current Page => {self.current_page_number}\nTotal Page => {self.total_pages}\nPage Display Value => {page_number_display.value}"
-                )
-
-        container: ft.Container = ft.Container(bgcolor=ft.colors.YELLOW_300)
-
-        table: ft.DataTable = ft.DataTable(
-            divider_thickness=1,
-        )
-        table.columns = self.columns
-
-        increase_button: ft.OutlinedButton = ft.OutlinedButton(
-            icon=ft.icons.SKIP_NEXT_OUTLINED,
-            on_click=_on_increase,
-        )
-        decrease_button: ft.OutlinedButton = ft.OutlinedButton(
-            icon=ft.icons.SKIP_PREVIOUS_OUTLINED,
-            on_click=_on_decrease,
-        )
-        page_number_display: ft.Text = ft.Text(
-            value=f"0 of 0",
-            style=ft.TextThemeStyle.LABEL_SMALL,
-        )
-
-        if len(self.rows) > 0:
-            self.current_page_number = 1
-            page_number_display.value = (
-                f"{self.current_page_number} of {self.total_pages}"
             )
-            table.rows = self.paginated_rows[self.current_page_number]
-        container_row_inside_column: ft.Row = ft.Row(
-            controls=[
-                decrease_button,
-                page_number_display,
-                increase_button,
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
+        self.rows = required_data
+        self.paginated_rows = self.paginate_rows()
+        self.set_a_page(page_number=self.current_page)
+        self.update()
+
+    def build(self):
+        card: ft.Card = ft.Card(
+            ft.Container(
+                ft.Column(
+                    controls=[
+                        self.title_text,
+                        self.refresh_data_button,
+                        self.data_table,
+                        ft.Row(controls=[]),
+                    ]
+                )
+            )
         )
-        container_column: ft.Column = ft.Column(
-            controls=[
-                table,
-                container_row_inside_column,
-            ],
-            alignment=ft.MainAxisAlignment.SPACE_EVENLY,
-        )
-        return container_column
 
 
-def main(page):
-    col = [ft.DataColumn(ft.Text("Sno")), ft.DataColumn(ft.Text("Name"))]
+def main(page: ft.Page):
+    cols = [
+        ft.DataColumn(ft.Text("S.NO")),
+        ft.DataColumn(ft.Text("Name")),
+    ]
     rows = []
-    for i in range(100):
+
+    for i in range(76):
         rows.append(
             ft.DataRow(
-                cells=[
-                    ft.DataCell(ft.Text(f"{i+1}.")),
-                    ft.DataCell(ft.Text("God")),
-                ]
+                cells=[ft.DataCell(ft.Text(f"{i+1}.")), ft.DataCell(ft.Text("God"))]
             )
         )
-    page.add(PaginatedDataTable(columns=col, rows=rows))
+    # table = ft.DataTable(columns=cols, rows=rows)
+    page.theme_mode = ft.ThemeMode.DARK
+    page.add(PaginatedDataTable(columns=cols, rows=rows))
 
 
 ft.app(target=main)
--- simple_ledger/fletter/fletter2.py
+++ simple_ledger/fletter/fletter2.py
@@ -2,7 +2,7 @@ import flet as ft
 
 from simple_ledger.db import Ledger, LedgerDB
 
-# from simple_ledger.fletter.controls import PaginatedDataTable
+from simple_ledger.fletter.controls import PaginatedDataTable
 
 
 def main(page: ft.Page):
@@ -27,7 +27,7 @@ def main(page: ft.Page):
             )
         )
 
-    data_table = ft.DataTable(
+    data_table: ft.DataTable = ft.DataTable(
         columns=[
             ft.DataColumn(label=ft.Text("S.No"), numeric=True),
             ft.DataColumn(label=ft.Text("From")),
@@ -39,6 +39,27 @@ def main(page: ft.Page):
         expand=True,
     )
 
+    def on_changing_tab(e):
+        required_data = []
+
+        for data in database.read_ledger_info():
+            required_data.append(
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text(value=str(data.id))),
+                        ft.DataCell(ft.Text(value=str(data.from_person))),
+                        ft.DataCell(ft.Text(value=str(data.to_person))),
+                        ft.DataCell(ft.Text(value=str(data.amount))),
+                        ft.DataCell(ft.Text(value=str(data.tag))),
+                    ]
+                )
+            )
+        data_table.rows = required_data
+        # paginated_data_table.dt = data_table
+        page.update(data_table)
+        # page.update(paginated_data_table)
+        page.update(summary_tab)
+
     # summary_tab = ft.ListView(auto_scroll=True, expand=1, spacing=10)
     # summary_tab.controls.append(data_table)
 
@@ -47,23 +68,21 @@ def main(page: ft.Page):
     # !-- Tab TWO
 
     summary_tab = ft.Column(
-        controls=[data_table],
+        controls=[paginated_data_table],
         expand=True,
         scroll=ft.ScrollMode.AUTO,
     )
 
-    secondary_column_from = ft.TextField(
-        label="From", autofocus=True, height=50
-    )
+    secondary_column_from = ft.TextField(label="Given From", autofocus=True, height=50)
 
-    secondary_column_to = ft.TextField(label="To", height=50)
-    secondary_column_amount = ft.TextField(label="Amount", height=50)
-    secondary_column_description = ft.TextField(label="Description", height=50)
+    secondary_column_to = ft.TextField(label="Given To", height=50)
+    secondary_column_amount = ft.TextField(label="Given Amount", height=50)
+    secondary_column_description = ft.TextField(label="Transaction Info", height=50)
     secondary_column_tag = ft.Dropdown(
-        label="Tag",
+        label="Mode Of Transaction",
         options=[
-            ft.dropdown.Option(key="CREDIT", text="CREDIT"),
-            ft.dropdown.Option(key="DEBIT", text="DEBIT"),
+            ft.dropdown.Option(key="CREDIT", text="Credit"),
+            ft.dropdown.Option(key="DEBIT", text="Debit"),
         ],
     )
 
@@ -76,7 +95,7 @@ def main(page: ft.Page):
             secondary_column_tag,
         ]:
             if not val.value:
-                print("None  | False")
+                ...  # do nothing / log to console that there is no data to add
         else:
             entry = Ledger(
                 from_person=str(secondary_column_from.value),
@@ -86,11 +105,16 @@ def main(page: ft.Page):
                 tag=secondary_column_tag.value.upper(),
             )
             database.add_ledger_info(ledger=entry)
-            # secondary_column_from.value = ""
-            # secondary_column_to.value = ""
-            # secondary_column_amount.value = ""
-            # secondary_column_description.value = ""
-            # secondary_column_tag.value = ""
+            secondary_column_from.value = ""
+            secondary_column_to.value = ""
+            secondary_column_amount.value = ""
+            secondary_column_description.value = ""
+            secondary_column_tag.value = ""
+            page.update(secondary_column_from)
+            page.update(secondary_column_to)
+            page.update(secondary_column_amount)
+            page.update(secondary_column_description)
+            page.update(secondary_column_tag)
 
     ledger_entry_button = ft.TextButton(text="+ Add", on_click=add_entry)
 
@@ -122,12 +146,10 @@ def main(page: ft.Page):
             ),
             ft.Tab(icon=ft.icons.DELETE_ROUNDED, text="Delete Entry"),
         ],
+        on_change=on_changing_tab,
     )
 
     page.add(tab)
 
 
-if ft.Page.platform == "android":
-    ft.app(target=main, view=ft.WEB_BROWSER, port=9000)
-else:
-    ft.app(target=main)
+ft.app(target=main)
